db0048e47b4c75748f30c9518b5d1499842d3deb
GresQu <gramwgre3221@gmail.com>
2026-01-12 15:06:26 +0100

    Refactor AutoFollowName.lua for improved functionality

--- vBot/AutoFollowName.lua
+++ vBot/AutoFollowName.lua
@@ -1,52 +1,37 @@
 local sep = UI.Separator()
 sep:setHeight(4)
 sep:setBackgroundColor('#A0B0C0')
-followName = "autofollow"
-if not storage[followName] then storage[followName] = { player = 'name'} end
-local toFollowPos = {}
-
-UI.Label("Auto Follow")
-
-followTE = UI.TextEdit(storage[followName].player or "name", function(widget, newText)
-    storage[followName].player = newText
+UI.Separator()
+UI.Label("Auto Follow Name")
+addTextEdit("followleader", storage.followLeader or "player name", function(widget, text)
+    storage.followLeader = text
 end)
 
-local followChange = macro(20, "Follow Change", function() end)
-
-local followMacro = macro(20, "Follow", function()
-    local target = getCreatureByName(storage[followName].player)
+-- Code
+local toFollowPos = {}
+local followMacro = macro(1, "Follow", function()
+    local target = getCreatureByName(storage.followLeader)
     if target then
         local tpos = target:getPosition()
         toFollowPos[tpos.z] = tpos
     end
+
     if player:isWalking() then
         return
     end
+
     local p = toFollowPos[posz()]
     if not p then
         return
     end
-    if autoWalk(p, 20, { ignoreNonPathable = true, precision = 1 }) then
-        delay(20)
+
+    if autoWalk(p, 20, {ignoreNonPathable=true, precision=1}) then
+        delay(50)
     end
 end)
 
-onPlayerPositionChange(function(newPos, oldPos)
-  if followChange:isOff() then return end
-  if (g_game.isFollowing()) then
-    tfollow = g_game.getFollowingCreature()
-
-    if tfollow then
-      if tfollow:getName() ~= storage[followName].player then
-        followTE:setText(tfollow:getName())
-        storage[followName].player = tfollow:getName()
-      end
-    end
-  end
-end)
-
-onCreaturePositionChange(function(creature, newPos, oldPos)
-    if creature:getName() == storage[followName].player and newPos then
+onCreaturePositionChange(function(creature, oldPos, newPos)
+    if creature:getName() == storage.followLeader then
         toFollowPos[newPos.z] = newPos
     end
 end)
